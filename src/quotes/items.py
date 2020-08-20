b57fb86216b02c8c8acf81e4fbcfbc9f3b4567d0
Miguel Osuna <osuna.angulo.miguel@gmail.com>
2020-08-20 14:47:00 -0700

    Remove preprocessing and postprocessing from spider and add it to QuotesItem in items.py

--- src/quotes/items.py
+++ src/quotes/items.py
@@ -1,12 +1,79 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
-import scrapy
+import re
+from scrapy.item import Item, Field
+from scrapy.loader.processors import Compose, MapCompose, TakeFirst
 
 
-class QuotesItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+def sanitize_quote(quote_list):
+    """ Sanitizes the text from the quote string list. """
+    quote_str = " ".join(quote_list)
+    regex = re.compile("\u201c(.+?)\u201d")
+    text = re.search(regex, quote_str)
+
+    # If there was a match with the regular expressions
+    if text is not None:
+        text = text.group().strip(u"\u201c" u"\u201d")
+        return text
+    else:
+        return quote_str
+
+
+def sanitize_author(author_str):
+    """ Sanitizes the text from the author string. """
+    return " ".join(author_str.split())
+
+
+def sanitize_image(author_img_url):
+    """ Sanitizes the text from the author string. """
+    if author_img_url is None:
+        author_img_url = "https://www.pngkey.com/png/detail/230-2301779_best-classified-apps-default-user-profile.png"
+    return author_img_url
+
+
+def sanitize_quote_tags(tag_list):
+    allowed_tag_categories = [
+        "love",
+        "life",
+        "inspiration",
+        "humor",
+        "philosophy",
+        "god",
+        "truth",
+        "widsom",
+        "romance",
+        "poetry",
+        "death",
+        "happiness",
+        "hope",
+        "faith",
+        "religion",
+        "life-lessons",
+        "success",
+        "motivational",
+        "time",
+        "knowledge",
+        "love",
+        "spirituality",
+        "science",
+        "books",
+    ]
+
+    """ Sanitizes and filters the tags comparing them to the allowed quote tags. """
+    quote_tags = []
+    for tag in tag_list:
+        if tag in allowed_tag_categories:
+            quote_tags.append(tag)
+
+    return quote_tags if len(quote_tags) else ["other"]
+
+
+class QuotesItem(Item):
+    quote_content = Field(
+        input_processor=Compose(sanitize_quote), output_processor=TakeFirst()
+    )
+    author_name = Field(
+        input_processor=MapCompose(sanitize_author), output_processor=TakeFirst()
+    )
+    author_image = Field(
+        input_processor=MapCompose(sanitize_image), output_processor=TakeFirst()
+    )
+    tags = Field(input_processor=Compose(sanitize_quote_tags))
--- src/quotes/spiders/quotes_spider.py
+++ src/quotes/spiders/quotes_spider.py
@@ -1,71 +1,19 @@
-import scrapy
-import re
+# Standard library imports
+from scrapy import Spider
+from scrapy.loader import ItemLoader
+
+# Local application imports
+from quotes.items import QuotesItem
+
+quote_item = QuotesItem()
 
 
-class QuotesSpider(scrapy.Spider):
+class QuotesSpider(Spider):
     """ Web Scraping Spider for Goodreads website. """
 
     # Class attributes
     name = "quotes"
     start_urls = ["https://www.goodreads.com/quotes?page=1"]
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.allowed_tag_categories = [
-            "love",
-            "life",
-            "inspiration",
-            "humor",
-            "philosophy",
-            "god",
-            "truth",
-            "widsom",
-            "romance",
-            "poetry",
-            "death",
-            "happiness",
-            "hope",
-            "faith",
-            "religion",
-            "life-lessons",
-            "success",
-            "motivational",
-            "time",
-            "knowledge",
-            "love",
-            "spirituality",
-            "science",
-            "books",
-        ]
-
-    def sanitize_quote(self, quote_str):
-        """ Sanitizes the text from the quote string. """
-        regex = re.compile("\u201c(.+?)\u201d")
-        text = re.search(regex, quote_str)
-
-        if text is not None:
-            return text.group()
-        else:
-            return " ".join(quote_str.split())
-
-    def sanitize_author(self, author_str):
-        """ Sanitizes the text from the author string. """
-        return " ".join(author_str.split())
-
-    def sanitize_image(self, author_img_url):
-        """ Sanitizes the text from the author string. """
-        if author_img_url is None:
-            author_img_url = "https://www.pngkey.com/png/detail/230-2301779_best-classified-apps-default-user-profile.png"
-        return author_img_url
-
-    def sanitize_quote_tags(self, tags):
-        """ Sanitizes and filters the tags comparing them to the allowed quote tags. """
-        quote_tags = []
-        for tag in tags:
-            if tag in self.allowed_tag_categories:
-                quote_tags.append(tag)
-
-        return quote_tags if len(quote_tags) else ["other"]
 
     def parse(self, response):
         """ Parsing function for the spider's requests. """
@@ -73,26 +21,22 @@ class QuotesSpider(scrapy.Spider):
         # Remove the line breaks on the html
         response = response.replace(body=response.body.replace(b"<br>", b""))
 
-        for quote in response.css("div.quoteDetails"):
-            text = quote.css("div.quoteText *::text").get()
-            authorName = quote.css("div.quoteText span.authorOrTitle::text").get()
-            authorImage = quote.css("a.leftAlignedImage img::attr(src)").get()
-            tags = quote.css(
-                "div.quoteFooter div.greyText.smallText.left a::text"
-            ).getall()
+        for quote in response.css(".quoteDetails"):
+            # Create an item loader with the quote data and add it as a new quote_item
 
-            # Sanitize the data scraped
-            self.logger.info("Sanitizing data")
-            text = self.sanitize_quote(text)
-            authorName = self.sanitize_author(authorName)
-            authorImage = self.sanitize_image(authorImage)
-            tags = self.sanitize_quote_tags(tags)
+            self.logger.info("Creating quote_item")
 
-            yield dict(
-                text=text, authorName=authorName, authorImage=authorImage, tags=tags,
-            )
+            loader = ItemLoader(item=QuotesItem(), selector=quote)
+            loader.add_css("quote_content", ".quoteText::text")
+            print(loader.get_css(".quoteText::text"))
+            loader.add_css("author_name", ".quoteText .authorOrTitle::text")
+            loader.add_css("author_image", ".leftAlignedImage img::attr(src)")
+            loader.add_css("tags", ".greyText.smallText.left a::text")
+            quote_item = loader.load_item()
 
-            # Scrape the next page
-            next_page = response.css("a.next_page::attr(href)").get()
-            if next_page is not None:
-                yield response.follow(next_page, self.parse)
+            yield quote_item
+
+            # # Scrape the next page
+            # next_page = response.css("a.next_page::attr(href)").get()
+            # if next_page is not None:
+            #     yield response.follow(next_page, self.parse)
