4133498c6ceb692af891f7d426e96d15c48f24c2
Miguel Osuna <osuna.angulo.miguel@gmail.com>
2020-08-19 22:19:47 -0700

    Modify quote spider

--- src/quotes/spiders/quotes_spider.py
+++ src/quotes/spiders/quotes_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+import re
 
 
 class QuotesSpider(scrapy.Spider):
@@ -39,11 +40,23 @@ class QuotesSpider(scrapy.Spider):
 
     def sanitize_quote(self, quote_str):
         """ Sanitizes the text from the quote string. """
-        pass
+        regex = re.compile("\u201c(.+?)\u201d")
+        text = re.search(regex, quote_str)
+
+        if text is not None:
+            return text.group()
+        else:
+            return " ".join(quote_str.split())
 
     def sanitize_author(self, author_str):
         """ Sanitizes the text from the author string. """
-        return author_str.replace(",", "")
+        return " ".join(author_str.split())
+
+    def sanitize_image(self, author_img_url):
+        """ Sanitizes the text from the author string. """
+        if author_img_url is None:
+            author_img_url = "https://www.pngkey.com/png/detail/230-2301779_best-classified-apps-default-user-profile.png"
+        return author_img_url
 
     def sanitize_quote_tags(self, tags):
         """ Sanitizes and filters the tags comparing them to the allowed quote tags. """
@@ -57,17 +70,22 @@ class QuotesSpider(scrapy.Spider):
     def parse(self, response):
         """ Parsing function for the spider's requests. """
 
+        # Remove the line breaks on the html
+        response = response.replace(body=response.body.replace(b"<br>", b""))
+
         for quote in response.css("div.quoteDetails"):
-            text = quote.css("div.quoteText::text").get()
+            text = quote.css("div.quoteText *::text").get()
             authorName = quote.css("div.quoteText span.authorOrTitle::text").get()
             authorImage = quote.css("a.leftAlignedImage img::attr(src)").get()
             tags = quote.css(
                 "div.quoteFooter div.greyText.smallText.left a::text"
             ).getall()
 
-            # Parse and clean
+            # Sanitize the data scraped
+            self.logger.info("Sanitizing data")
             text = self.sanitize_quote(text)
             authorName = self.sanitize_author(authorName)
+            authorImage = self.sanitize_image(authorImage)
             tags = self.sanitize_quote_tags(tags)
 
             yield dict(
@@ -78,4 +96,3 @@ class QuotesSpider(scrapy.Spider):
             next_page = response.css("a.next_page::attr(href)").get()
             if next_page is not None:
                 yield response.follow(next_page, self.parse)
-
